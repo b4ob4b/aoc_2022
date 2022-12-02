29b06f7d0e2f43c6e6f3270da81f8665c05602df
Julian Baumgartner <julian.baumgaertner@bmw.de>
2022-12-02 13:24:48 +0100

    Day02 cleanup

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -1,6 +1,5 @@
 import utils.Day
 import utils.IO
-import utils.print
 import utils.splitLines
 
 fun main() {
@@ -9,48 +8,39 @@ fun main() {
 
 class Day02: Day(inputType = IO.TYPE.INPUT) {
 
-    val map = mapOf(
-        Pair(Pair("A","X"),1+3),
-        Pair(Pair("A","Y"),2+6),
-        Pair(Pair("A","Z"),3+0),
-        Pair(Pair("B","X"),1+0),
-        Pair(Pair("B","Y"),2+3),
-        Pair(Pair("B","Z"),3+6),
-        Pair(Pair("C","X"),1+6),
-        Pair(Pair("C","Y"),2+0),
-        Pair(Pair("C","Z"),3+3)
-    )
-// X losa
-//    Y draw
-//    Z win
-    val map2 = mapOf(
-//                  object,  losw
-        Pair(Pair("A","X"),3+0),
-        Pair(Pair("A","Y"),1+3),
-        Pair(Pair("A","Z"),2+6),
-        Pair(Pair("B","X"),1+0),
-        Pair(Pair("B","Y"),2+3),
-        Pair(Pair("B","Z"),3+6),
-        Pair(Pair("C","X"),2+0),
-        Pair(Pair("C","Y"),3+3),
-        Pair(Pair("C","Z"),1+6)
+    private val scoresGame1 = mapOf(
+        Pair('A','X') to 1+3,
+        Pair('A','Y') to 2+6,
+        Pair('A','Z') to 3+0,
+        Pair('B','X') to 1+0,
+        Pair('B','Y') to 2+3,
+        Pair('B','Z') to 3+6,
+        Pair('C','X') to 1+6,
+        Pair('C','Y') to 2+0,
+        Pair('C','Z') to 3+3
     )
 
-    val data = input.splitLines()
-        .map { Pair(it.split(" ").first(), it.split(" ")[1]) }
+    private val scoresGame2 = mapOf(
+        Pair('A','X') to 3+0,
+        Pair('A','Y') to 1+3,
+        Pair('A','Z') to 2+6,
+        Pair('B','X') to 1+0,
+        Pair('B','Y') to 2+3,
+        Pair('B','Z') to 3+6,
+        Pair('C','X') to 2+0,
+        Pair('C','Y') to 3+3,
+        Pair('C','Z') to 1+6
+    )
 
-    override fun part1(): Any? {
-       return  data.map {
-            map.get(it)
-        }.sumOf { it!! }
-        return "not yet implement"
+    val games = input.splitLines()
+        .map { it.split(" ").map(String::first) }
+        .map { (opponent,me) -> Pair(opponent, me) }
+
+    override fun part1(): Int {
+       return games.mapNotNull(scoresGame1::get).sum()
     }
 
-
-    override fun part2(): Any? {
-        return  data.map {
-            map2.get(it)
-        }.sumOf { it!! }
-        return "not yet implement"
+    override fun part2(): Int {
+        return games.mapNotNull(scoresGame2::get).sum()
     }
 }           
