58a220535a0cf35715575e85a58f7f3e4fea029b
Julian Baumgartner <julian.baumgaertner@bmw.de>
2022-12-03 08:21:31 +0100

    Day03 cleaned up

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -3,26 +3,28 @@ import utils.IO
 import utils.splitLines
 
 fun main() {
-    Day03(IO.TYPE.SAMPLE).test(157,70)
+    Day03(IO.TYPE.SAMPLE).test(157, 70)
     Day03().solve()
 }
 
-class Day03(inputType: IO.TYPE = IO.TYPE.INPUT): Day(inputType = inputType) {
+class Day03(inputType: IO.TYPE = IO.TYPE.INPUT) : Day(inputType = inputType) {
+
+    private val rucksacks = input.splitLines()
+    private fun Char.calculatePriority() = (('a'..'z') + ('A'..'Z')).indexOf(this) + 1
+
     override fun part1(): Int {
-        return input.splitLines().map { it.toCharArray().asList() }
-            .map { Pair(it.subList(0, it.size / 2).toSet(),it.subList(it.size / 2, it.size).toSet()) }
-            .map { it.first.intersect(it.second) }
-            .map { it.single().toInt() - 96 }
-            .map { if(it < 0) it + 58 else it }
+        return rucksacks.asSequence().map { it.toList() }
+            .map { it.chunked(it.size / 2).map { it.toSet() } }
+            .map { (compartment1, compartment2) -> compartment1.intersect(compartment2) }
+            .map { it.single().calculatePriority() }
             .sum()
     }
 
     override fun part2(): Int {
-        return input.splitLines().chunked(3).map {
-            it.map { it.toCharArray() }
-                .reduce { acc, chars -> acc.intersect(chars.toList()).toCharArray() }
-                .map { it.toString().toCharArray().single().toInt()  - 96 }
-                .map { if(it < 0) it + 58 else it }
-        }.flatten().sum()
+        return rucksacks.chunked(3).flatMap { group ->
+            group.map { it.toCharArray().toSet() }
+                .reduce { acc, chars -> acc.intersect(chars) }
+                .map { it.calculatePriority() }
+        }.sum()
     }
 }           
