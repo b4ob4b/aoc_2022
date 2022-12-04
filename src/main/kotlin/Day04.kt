28f19a47362d4550b9b32f038c3ed01eb044adcc
Julian Baumgartner <julian.baumgaertner@bmw.de>
2022-12-04 09:06:34 +0100

    Day04 - cleaned up

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -3,30 +3,23 @@ import utils.IO
 import utils.splitLines
 
 fun main() {
-    Day04(IO.TYPE.SAMPLE).test(2,4)
+    Day04(IO.TYPE.SAMPLE).test(2, 4)
     Day04().solve()
 }
 
 class Day04(inputType: IO.TYPE = IO.TYPE.INPUT) : Day(inputType = inputType) {
-    override fun part1(): Int {
-        return input.splitLines()
-            .map {
-                it.split(",")
-                    .map {
-                        it.split("-").map { it.toInt() }
-                    }
-                    .map { (it[0]..it[1]).toSet() }
-            }.count { sets -> sets[0].containsAll(sets[1]) || sets[1].containsAll(sets[0]) }
+
+    private val sections = input.splitLines().parseSections()
+
+    private fun List<String>.parseSections() = this
+        .map { it.split(Regex(",|-")).map(String::toInt) }
+        .map { (a, b, c, d) -> Pair(a..b, c..d) }
+
+    private infix fun IntRange.containsAll(other: IntRange) = other.first >= this.first && other.last <= this.last
+
+    override fun part1() = sections.count { (range1, range2) ->
+        range1 containsAll range2 || range2 containsAll range1
     }
 
-    override fun part2(): Int {
-        return input.splitLines()
-            .map {
-                it.split(",")
-                    .map {
-                        it.split("-").map { it.toInt() }
-                    }
-                    .map { (it[0]..it[1]).toSet() }
-            }.count { sets -> sets[0].fold(false) { acc, i -> acc || sets[1].contains(i) } }
-    }
+    override fun part2() = sections.count { (range1, range2) -> (range1 intersect range2).isNotEmpty() }
 }           
