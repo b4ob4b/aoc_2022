a0975834832ed102fdc7530540f6901f05b3e7e0
Julian Baumgartner <julian.baumgaertner@bmw.de>
2022-12-06 07:18:30 +0100

    Day06 cleaned up

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -1,33 +1,25 @@
 import utils.Day
 import utils.IO
-import utils.print
 
 fun main() {
     Day06(IO.TYPE.SAMPLE).test(7,19)
     Day06().solve()
 }
 
-class Day06(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("", inputType = inputType) {
+class Day06(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("Tuning Trouble", inputType = inputType) {
+
+    private val signal = input.asSequence()
     
+    private fun Sequence<Char>.calculateSignalStart(startOfPacketSize: Int) = this
+        .windowed(startOfPacketSize)
+        .indexOfFirst { it.toSet().size == startOfPacketSize }
+        .let { it + startOfPacketSize }
+
     override fun part1(): Any? {
-        return input.toCharArray().toList().windowed(4).map { it.toSet() }
-            .mapIndexed { index, chars -> 
-            if (chars.size == 4) index else null
-        }
-            .filterNotNull()
-            .first()
-            .let { it+4 }
+        return signal.calculateSignalStart(4)
     }
 
     override fun part2(): Any? {
-        return input.toCharArray().toList().windowed(14).map { it.toSet() }
-            .also { it.print() }
-            .also { it.map { it.size }.print() }
-            .mapIndexed { index, chars ->
-                if (chars.size == 14) index else null
-            }
-            .filterNotNull()
-            .first()
-            .let { it+14 }
+        return signal.calculateSignalStart(14)
     }
 }           
