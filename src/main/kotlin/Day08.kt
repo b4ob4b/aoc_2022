6befe24403845a6dee022021707b7f2a45c2b693
Julian Baumgartner <julian.baumgaertner@bmw.de>
2022-12-08 21:47:59 +0100

    Day08 cleanup #2

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -1,5 +1,4 @@
 import utils.*
-import kotlin.math.abs
 
 fun main() {
     Day08(IO.TYPE.SAMPLE).test(21, 8)
@@ -8,78 +7,64 @@ fun main() {
 
 class Day08(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("", inputType = inputType) {
 
-    private val trees = input.splitLines().map { it.toList().map { it.toString().toInt() } }
-
-    data class Edge(val rows: IntRange, val cols: IntRange, val watchDirection: Position)
+    private val trees = input.splitLines().map { rows -> rows.toList().map { cell -> cell.toString().toInt() } }
 
     private val maxEdge = trees.size - 1
     private val visibleTrees = mutableSetOf<Position>()
     private val edge = 1 until maxEdge
-    private val edges = listOf(
-        Edge(0.toSingleRange(), edge, Position(1, 0)), // upper, watch down
-        Edge(edge, maxEdge.toSingleRange(), Position(0, -1)), // right, watch left
-        Edge(maxEdge.toSingleRange(), edge, Position(-1, 0)), // lower, watch up
-        Edge(edge, 0.toSingleRange(), Position(0, 1)), // left, watch right
+
+    private val viewLines = listOf(
+        edge.map { x -> (0..maxEdge).map { y -> Position(x, y) }.asSequence() }, // left to right
+        edge.map { y -> (0..maxEdge).map { x -> Position(x, y) }.asSequence() }, // top to bottom
+        edge.map { x -> (maxEdge downTo 0).map { y -> Position(x, y) }.asSequence() }, // right to left
+        edge.map { y -> (maxEdge downTo 0).map { x -> Position(x, y) }.asSequence() }, // bottom to top
     )
 
     override fun part1(): Int {
-        edges.forEach { (rows, cols, direction) ->
-            rows.forEach { row ->
-                cols.forEach { col ->
-                    val traverseTrees = (edge.first..edge.last).iterator()
-                    var treeSize: Int = trees[row][col]
-                    var nextRow: Int
-                    var nextCol: Int
-                    var nextTreeSize: Int
-
-                    do {
-                        nextRow = row + if (direction.x != 0) direction.x * traverseTrees.nextInt() else 0
-                        nextCol = col + if (direction.y != 0) direction.y * traverseTrees.nextInt() else 0
-                        nextTreeSize = trees[nextRow][nextCol]
-                        if (nextTreeSize > treeSize) {
-                            visibleTrees.add(Position(nextRow, nextCol))
-                            treeSize = nextTreeSize
-                        }
-                    } while (traverseTrees.hasNext())
+        val amountCorners = 4
+        visibleTrees.addAll(
+            viewLines.flatMap { viewLine ->
+                viewLine.flatMap { positions ->
+                    positions.lookForBiggerTrees()
                 }
             }
-        }
-        val amountCorners = 4
-        val amountEdges = edge.toList().size * 4
-        return visibleTrees.size + amountCorners + amountEdges
-    }
-
-    private fun Int.toSingleRange() = this..this
-
-    private fun List<List<Int>>.getViewDistanceOf(treePosition: Position, direction: Direction): Int {
-        val movement = when (direction) {
-            Direction.up -> (treePosition.x - 1) downTo 0 to treePosition.y.toSingleRange()
-            Direction.right -> treePosition.x.toSingleRange() to (treePosition.y + 1)..maxEdge
-            Direction.down -> (treePosition.x + 1)..maxEdge to treePosition.y.toSingleRange()
-            Direction.left -> treePosition.x.toSingleRange() to ((treePosition.y - 1) downTo 0)
-        }
-        val treeSize = this[treePosition.x][treePosition.y]
-        val (rows, cols) = movement
-        val isSmaller = rows.flatMap { row ->
-            cols.map { col ->
-                this[row][col] < treeSize
-            }
-        }.takeWhile { it }
-        return if (isSmaller.size <= listOf(rows, cols).maxOf { abs(it.last - it.first) })
-            isSmaller.size + 1
-        else
-            isSmaller.size
-
+        )
+        return visibleTrees.size + amountCorners
     }
 
     override fun part2(): Int {
-        return visibleTrees.map { treePosition ->
-            listOf(
-                trees.getViewDistanceOf(treePosition, Direction.up),
-                trees.getViewDistanceOf(treePosition, Direction.right),
-                trees.getViewDistanceOf(treePosition, Direction.down),
-                trees.getViewDistanceOf(treePosition, Direction.left),
-            )
-        }.maxOf { it.product() }
+        return visibleTrees
+            .filter { it.x != 0 && it.x != maxEdge && it.y != 0 && it.y != maxEdge }
+            .maxOf { treePosition ->
+                treePosition.getViewLines().map {
+                    it.getViewRange(treePosition)
+                }.product()
+            }
     }
+
+    private fun Sequence<Position>.lookForBiggerTrees() = this.takeWhile { trees[it.x][it.y] <= 9 }
+        .fold(emptySet<Position>()) { visibleTrees, position ->
+            if (visibleTrees.isEmpty()) return@fold setOf(position)
+            val sizeBiggestTree = visibleTrees.last().let { trees[it.x][it.y] }
+            if (trees[position.x][position.y] > sizeBiggestTree) {
+                visibleTrees + position
+            } else {
+                visibleTrees
+            }
+        }
+
+
+    private fun Position.getViewLines() = listOf(
+        ((this.y + 1)..maxEdge).map { y -> Position(this.x, y) }, // left to right
+        ((this.x + 1)..maxEdge).map { x -> Position(x, this.y) }, // top to bottom
+        ((this.y - 1) downTo 0).map { y -> Position(this.x, y) }, // right to left
+        ((this.x - 1) downTo 0).map { x -> Position(x, this.y) }, // bottom to top
+    )
+
+    private fun List<Position>.getViewRange(treePosition: Position): Int {
+        val viewHeight = trees[treePosition.x][treePosition.y]
+        val viewRange = this.takeWhile { trees[it.x][it.y] < viewHeight }.count()
+        return if (viewRange < this.size) viewRange + 1 else viewRange
+    }
+
 }           
