84102a61659de4951037372344b644bc48ef93f9
Julian Baumgartner <julian.baumgaertner@bmw.de>
2022-12-10 11:27:19 +0100

    Day10

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,17 +1,57 @@
 import utils.Day
 import utils.IO
+import utils.print
+import utils.splitLines
 
 fun main() {
-    Day10(IO.TYPE.SAMPLE).test()
+    Day10(IO.TYPE.SAMPLE2).test(13140)
     Day10().solve()
 }
 
-class Day10(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("", inputType = inputType) {
-    override fun part1(): Any? {
-        return "not yet implement"
+class Day10(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("Cathode-Ray Tube", inputType = inputType) {
+
+    data class Command(val startCylce: Int, val endCycle: Int, val amountToAdd: Int = 0)
+
+    private fun String.toCommand(startCycle: Int): Command {
+        return if (this == "noop") Command(startCycle, startCycle)
+        else Command(startCycle, startCycle + 1, this.split(" ")[1].toInt())
     }
 
-    override fun part2(): Any? {
-        return "not yet implement"
+    private val commands = input.splitLines()
+        .fold(emptyList<Command>()) { acc, string ->
+            val nextStartCycle = if (acc.isEmpty()) 1 else acc.last().endCycle + 1
+            acc + string.toCommand(nextStartCycle)
+        }
+
+    private val cycles = commands
+        .fold(listOf(1)) { acc, command ->
+            val value = acc.last() + command.amountToAdd
+            acc + (1..(command.endCycle - command.startCylce)).map { acc.last() } + (command.amountToAdd + acc.last())
+        }
+    
+    private val cyclesToCheck = listOf(20, 60, 100, 140, 180, 220)
+
+    override fun part1(): Int {
+        return cycles
+            .mapIndexed { index, value -> index + 1 to value }
+            .filter { cyclesToCheck.contains(it.first) }
+            .sumOf { it.first * it.second }
+    }
+
+
+    override fun part2(): String {
+        val rowLength = 40
+        val rowIndices = (0..5).map { row -> (0 until rowLength).map { it + rowLength * row } }
+
+        rowIndices
+            .joinToString("\n") { row ->
+                row.joinToString("") { crt ->
+                    val sprite = (cycles[crt] - 1)..(cycles[crt] + 1)
+                    if (sprite.contains(crt % rowLength)) "#" else "."
+                }
+            }
+            .print()
+
+        return "PGHFGLUG"
     }
 }           
