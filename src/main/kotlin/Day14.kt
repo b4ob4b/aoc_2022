267a768c3f327512b51661ceaae0560c9d138873
Julian Baumgartner <julian.baumgaertner@bmw.de>
2022-12-14 21:35:55 +0100

    Day14 cleanup

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -7,100 +7,97 @@ fun main() {
 
 class Day14(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("", inputType = inputType) {
 
-    private val sandReservoir = SandReservoir(input)
+    private val states = SandReservoir(input)
+        .dropsSand()
+        .takeWhile { it != SandReservoir.State.Full }
+        .toList()
+    
+    override fun part1(): Int {
+        return states.indexOfFirst { it == SandReservoir.State.Lost } 
+    }
+
+    override fun part2(): Int {
+        return states.count { it == SandReservoir.State.Resting }
+    }
 
     private class SandReservoir(wallInformation: String) {
-        private val walls = wallInformation
-            .splitLines()
-            .flatMap {
-                val corners = it
-                    .split(" -> ")
-                    .map { it.getPosition() }
-                corners.zipWithNext()
-                    .flatMap { it.getLine() }
-            }
-            .toSet()
+        private val rocks = wallInformation.splitLines().flatMap {
+                val corners = it.split(" -> ").map { it.getPosition() }
+                corners.zipWithNext().flatMap { it.getLine() }
+            }.toSet()
 
-        private val deepestWall = walls.maxOf { it.y }
+        private val deepestRock = rocks.maxOf { it.y }
         private val source = Position(500, 0)
-        private val offset = walls.minOf { it.x }
+        private val reservoirBottom = deepestRock + 2
         private val sandPositions = mutableSetOf<Position>()
         private val sand = Sand(source, State.Falling)
         private val down = Position(0, 1)
         private val downLeft = Position(-1, 1)
         private val downRight = Position(1, 1)
-        val numberOfSandDroplets
-            get() = sandPositions.size 
 
-        private fun Sand.isObject(position: Position): Boolean {
-            return (this.position + position) in sandPositions || (this.position + position) in walls
-        }
-
-        fun drop(): State {
+        private fun reset() {
             sand.position = source
             sand.state = State.Falling
-            while (sand.state !in listOf(State.Resting, State.Lost)) {
+        }
+
+
+        fun dropsSand() = sequence {
+            while (true) {
                 when (sand.state) {
-                    State.Falling -> {
-                        val fallingPath = sand.position.getPathThrough(down)
-                        val hitPosition =
-                            fallingPath.takeWhile { walls.contains(it).not() && sandPositions.contains(it).not() && it.y <= deepestWall }
-                                .last()
-                        if (hitPosition.y >= deepestWall) {
-                            sand.state = State.Lost
-                        } else {
-                            sand.position = hitPosition
-                            sand.state = State.Rolling
-                        }
-                    }
-                    State.Rolling -> {
-                        if (sand.isObject(downLeft).not()) {
-                            sand.position = sand.position + downLeft
-                            sand.state = State.Falling
-                        } else if (sand.isObject(downRight).not()) {
-                            sand.position = sand.position + downRight
-                            sand.state = State.Falling
-                        } else {
-                            sandPositions.add(sand.position)
-                            sand.state = State.Resting
-                        }
-                    }
-                    State.Resting -> {}
-                    State.Lost -> {}
+                    State.Falling -> sandFalls()
+                    State.Rolling -> sandRolls()
+                    State.Lost -> sand.state = State.Rolling
+                    else -> {}
                 }
             }
-            return sand.state
         }
 
-        private data class Sand(var position: Position, var state: State) {}
+        private suspend fun SequenceScope<State>.sandFalls() {
+            val hitPosition = sand.position.getPathThrough(down).takeWhile { it.touchesObstacle() }.last()
+            if (hitPosition.y >= deepestRock) yield(State.Lost)
+            sand.position = hitPosition
+            sand.state = State.Rolling
+        }
+        
+        private fun Position.touchesObstacle() = rocks.contains(this).not() && sandPositions.contains(this).not() && this.y < reservoirBottom
+        
+        private fun Sand.hitsObstacle(position: Position): Boolean {
+            return (this.position + position) in sandPositions || (this.position + position) in rocks || (this.position + position).y == reservoirBottom
+        }
+
+        private suspend fun SequenceScope<State>.sandRolls() {
+            if (sand.hitsObstacle(downLeft).not()) {
+                sandLooksLeft()
+            } else if (sand.hitsObstacle(downRight).not()) {
+                sandLooksRight()
+            } else {
+                sandFindsRestingPosition()
+            }
+        }
+
+        private fun sandLooksLeft() {
+            sand.position = sand.position + downLeft
+            sand.state = State.Falling
+        }
+
+        private fun sandLooksRight() {
+            sand.position = sand.position + downRight
+            sand.state = State.Falling
+        }
+
+        private suspend fun SequenceScope<State>.sandFindsRestingPosition() {
+            sandPositions.add(sand.position)
+            yield(State.Resting)
+            if (sand.position == source) {
+                yield(State.Full)
+            }
+            reset()
+        }
+
+        private data class Sand(var position: Position, var state: State)
 
         enum class State {
-            Falling, Rolling, Resting, Lost
+            Falling, Rolling, Resting, Lost, Full
         }
-
-        private fun Set<Position>.applyOffset() = this.map { it.applyOffset() }
-        private fun Position.applyOffset() = this - Position(offset, 0)
-
-        override fun toString(): String {
-            return (Matrix(walls.maxOf { it.x } - offset, deepestWall + 1) { "." })
-                .insertAt(walls.applyOffset().associateWith { "#" })
-                .insertAt(sandPositions.applyOffset().associateWith { "o" })
-                .insertAt(source.applyOffset(), "+")
-                .insertAt(Position(x=534, y=157).applyOffset(), "X")
-                .transpose().toString()
-        }
-    }
-
-
-    override fun part1(): Int {
-        var state: SandReservoir.State = SandReservoir.State.Falling
-        while (state != SandReservoir.State.Lost) {
-            state = sandReservoir.drop()
-        }
-        return sandReservoir.numberOfSandDroplets
-    }
-
-    override fun part2(): Any? {
-        return "not yet implement"
     }
 }           
