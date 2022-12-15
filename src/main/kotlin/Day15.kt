d37ac2516f3bb771d65846630760a078810280c9
Julian Baumgartner <julian.baumgaertner@bmw.de>
2022-12-15 21:04:04 +0100

    Day15

---
+++ src/main/kotlin/Day15.kt
@@ -0,0 +1,83 @@
+import utils.*
+import kotlin.math.abs
+
+fun main() {
+    Day15(IO.TYPE.SAMPLE).test(26, 56000011L)
+    Day15().solve()
+}
+
+class Day15(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("", inputType = inputType) {
+
+    data class Signal(val sensor: Position, val beacon: Position) {
+        val manhattenDistance
+            get() = (sensor - beacon).manhattenDistance
+    }
+
+    private val distressRow = if (isTest) 10 else 2_000_000
+    private val coordinateRange = if (isTest) 0..20 else 0..4_000_000
+
+    private val signals = input.splitLines()
+        .map { it.split("""at |:""".toRegex()) }
+        .map { (_, sensor, _, beacon) -> Signal(sensor.toPosition(), beacon.toPosition()) }
+
+    override fun part1(): Any? {
+        val affectedSignals = signals
+            .filter {
+                val y = it.sensor.y
+                val distance = abs(y - distressRow)
+                distance <= it.manhattenDistance
+            }
+
+        val distressPositions = affectedSignals.fold(emptySet<Position>()) { distessLine, signal ->
+            distessLine + listOf(Position.left, Position.right).flatMap { direction ->
+                Position(signal.sensor.x, distressRow)
+                    .getPathThrough(direction)
+                    .takeWhile { (it - signal.sensor).manhattenDistance <= signal.manhattenDistance }
+                    .toSet()
+            }
+        }
+
+        return (distressPositions - signals.map { it.beacon }.toSet()).size
+    }
+
+    override fun part2(): Long {
+        val possibleMissingBeaconPositions = signals
+            .flatMap { it.sensor.boundaryOf(it.manhattenDistance + 1) }
+            .filter { (x, y) -> x in coordinateRange && y in coordinateRange }
+            .toSet()
+        return possibleMissingBeaconPositions.first { possibleMissingBeacon ->
+            val inRange = signals.any { signal ->
+                val distancePointToSensor = (signal.sensor - possibleMissingBeacon).manhattenDistance
+                val signalStrength = signal.manhattenDistance + 1
+                val isPointInRange = distancePointToSensor < signalStrength
+                isPointInRange
+            }
+            !inRange
+        }.calculateTuningFrequency()
+    }
+
+    private fun Position.calculateTuningFrequency() = this.x * 4000000L + this.y
+
+    private fun Position.boundaryOf(distance: Int): Set<Position> {
+        val diagonalsNeighbours = listOf(
+            Position(1, 1),
+            Position(1, -1),
+            Position(-1, 1),
+            Position(-1, -1),
+        )
+        val visited = mutableSetOf<Position>()
+        val sensor = this
+        val queue = ArrayDeque<Position>()
+        queue.add(Position(sensor.x + distance, sensor.y))
+        while (queue.isNotEmpty()) {
+            val point = queue.removeFirst()
+            if (visited.contains(point)) continue
+            visited.add(point)
+            diagonalsNeighbours
+                .map { point + it }
+                .filter { (it - this).manhattenDistance == distance }
+                .forEach { queue.add(it) }
+        }
+        return visited
+    }
+}
