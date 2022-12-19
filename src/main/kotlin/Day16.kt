3a2a8baff5aa1636f303925b08efee7787ee9b3d
Julian Baumgartner <julian.baumgaertner@bmw.de>
2022-12-19 19:58:28 +0100

    Day16 try with dfs

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -1,4 +1,5 @@
 import utils.*
+import java.lang.Integer.max
 
 fun main() {
     Day16(IO.TYPE.SAMPLE).test(1651)
@@ -7,58 +8,69 @@ fun main() {
 
 class Day16(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("", inputType = inputType) {
 
-    data class Valve(
-        val name: String,
-        val rate: Int,
-        val tunnels: List<String>,
-        var openedAt: Int = 0,
-        var isOpen: Boolean = false
-    )
-
-    private val valves = input.splitLines()
-        .map {
-            it.split(" ").let { information ->
-                Valve(
-                    information[1],
-                    information[4].split("""=|;""".toRegex())[1].toInt(),
-                    information.slice(9 until information.size).map { it.removeSuffix(",") }
-                )
-            }
+    private val valves = input.splitLines().map {
+        it.split(" ").let { information ->
+            Valve(information[1],
+                information[4].split("""=|;""".toRegex())[1].toInt(),
+                information.slice(9 until information.size).map { it.removeSuffix(",") })
         }
+    }
 
-    override fun part1(): Any? {
-        val distances: MutableMap<String, MutableMap<String, Int>> = mutableMapOf()
+    private val distances: MutableMap<String, MutableMap<String, Int>> = mutableMapOf()
 
-        valves
-            .filter { it.rate > 0 || it.name == "AA" }
-            .forEach { startValve ->
+    init {
+        valves.filter { it.rate > 0 || it.name == "AA" }.forEach { startValve ->
 
-                distances[startValve.name] = mutableMapOf()
+            distances[startValve.name] = mutableMapOf()
 
-                val queue = ArrayDeque<Pair<Valve, Int>>()
-                val start = valves.single { it.name == "AA" }
+            val queue = ArrayDeque<Pair<Valve, Int>>()
+            queue.add(startValve to 0)
 
-                queue.add(start to 0)
-                val visited = mutableSetOf<Valve>()
-                while (queue.isNotEmpty()) {
-                    val (valve, distance) = queue.removeFirst()
-                    visited.add(valve)
-                    valve.tunnels.map { name -> valves.single { it.name == name } }
-                        .filter { it !in visited }
-                        .forEach { neighbour ->
-                            queue.add(neighbour to distance + 1)
-                            distances[startValve.name]!![neighbour.name] = distance + 1
-                        }
+            val visited = mutableSetOf<Valve>()
+            while (queue.isNotEmpty()) {
+                val (valve, distance) = queue.removeFirst()
+                visited.add(valve)
+                valve.tunnels.map { name -> valves.single { it.name == name } }.filter { it !in visited }.forEach { neighbour ->
+                    queue.add(neighbour to distance + 1)
+                    if (neighbour.rate > 0) distances[startValve.name]!![neighbour.name] = distance + 1
                 }
             }
+        }
+    }
 
-
-        distances.print()
-
-        return "not yet implement"
+    override fun part1(): Int {
+        val states = valves.associate { it.name to State.Closed }
+        return dfs(30, "AA", states)
     }
 
     override fun part2(): Any? {
         return "not yet implement"
+    }
+
+    data class Valve(
+        val name: String,
+        val rate: Int,
+        val tunnels: List<String>,
+    )
+
+    enum class State { Open, Closed }
+
+    private fun dfs(
+        time: Int,
+        position: String,
+        states: Map<String, State>,
+    ): Int {
+        var maxFlow = 0
+
+        val states = states.toMutableMap()
+        val closedValves = distances[position]!!.filter { (name, _) -> states[name] == State.Closed }
+        for ((name, distance) in closedValves) {
+            val valve = valves.single { it.name == name }
+            val remainingTime = time - distance - 1
+            if (remainingTime <= 0) continue
+            states[name] = State.Open
+            maxFlow = max(maxFlow, dfs(remainingTime, valve.name, states) + remainingTime * valve.rate)
+        }
+        return maxFlow
     }
 }           
