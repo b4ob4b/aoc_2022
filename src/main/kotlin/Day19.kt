665e12d36851654ec7633638af199a2a2b631548
Julian Baumgartner <julian.baumgaertner@bmw.de>
2022-12-19 21:13:40 +0100

    Day19 try with dfs

--- src/main/kotlin/Day19.kt
+++ src/main/kotlin/Day19.kt
@@ -10,20 +10,55 @@ fun main() {
 }
 
 class Day19(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("", inputType = inputType) {
-
+    
     private val bluePrints = input.splitLines()
         .map { it.split(""": |\. |\.""".toRegex()).drop(1).dropLast(1) }
         .map { it.toBlueprint() }
 
-    data class Robot(val cost: Map<Mineral, Int>, val type: Mineral)
+    data class Robot(val cost: Map<Mineral, Int> = mapOf(), val type: Mineral)
 
     enum class Mineral { Ore, Clay, Obsidian, Geode }
 
+    enum class Choice { Buy, Save }
 
     override fun part1(): Any? {
-        bluePrints
-            .print()
+        dfs(bluePrints.first(), 24, listOf(Robot(type = Mineral.Ore)), emptyList()).print()
         return "not yet implement"
+    }
+
+    private fun dfs(blueprint: List<Robot>, time: Int, robots: List<Robot>, savings: List<Mineral>): Int {
+        // collect
+        val newSavings = robots.map { it.type } + savings
+        val remainingTime = time - 1
+
+        if (remainingTime == 0) return newSavings.filter { it == Mineral.Geode }.size
+
+        // choice
+        val robotsToBuy = blueprint.filter { blueprintRobot ->
+            blueprintRobot.cost.all { (mineral, amount) ->
+                val saving = newSavings.groupingBy { it }.eachCount()
+                if (mineral !in saving.keys) false
+                else newSavings.groupingBy { it }.eachCount()[mineral]!! >= amount
+            }
+        }
+
+        if (robotsToBuy.isEmpty()) return dfs(blueprint, remainingTime, robots, newSavings)
+
+        return Choice.values().maxOf { choice ->
+            when (choice) {
+                Choice.Save -> dfs(blueprint, remainingTime, robots, newSavings)
+                Choice.Buy -> {
+                    robotsToBuy.maxOf { robot ->
+                        val savingsAfterPurchase = newSavings.toMutableList().also { list ->
+                            robot.cost.forEach { (mineral, amount) ->
+                                repeat(amount) { list.remove(mineral) }
+                            }
+                        }
+                        dfs(blueprint, remainingTime, robots + robot, savingsAfterPurchase)
+                    }
+                }
+            }
+        }
     }
 
     override fun part2(): Any? {
