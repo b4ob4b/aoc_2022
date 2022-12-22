fa6520e08b4832993e43178cee5ad9a061c9cac2
Julian Baumgartner <julian.baumgaertner@bmw.de>
2022-12-22 04:07:06 +0100

    Day21 refactored

--- src/main/kotlin/Day21.kt
+++ src/main/kotlin/Day21.kt
@@ -7,126 +7,133 @@ fun main() {
     Day21().solve()
 }
 
-class Day21(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("", inputType = inputType) {
-
-    data class Monkey(val name: String, var result: Long? = null, val op1: String? = null, val op2: String? = null, val op: String? = null)
-
-    private fun getMokeys() = input.splitLines()
-        .map {
-            val words = it.split(" ")
-            val name = words[0].dropLast(1)
-            if (words.size == 2) {
-                Monkey(name, words[1].toLong())
-            } else {
-                Monkey(name, op1 = words[1], op2 = words.last(), op = words[2])
-            }
-        }
-
-    private fun String.doOperation(op1: Long, op2: Long) = when (this) {
-        "+" -> op1 + op2
-        "-" -> op1 - op2
-        "*" -> op1 * op2
-        else -> op1 / op2
-    }
-
-    private fun List<Monkey>.byName(name: String) = this.single { it.name == name }
+class Day21(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("Monkey Math", inputType = inputType) {
 
 
-    override fun part1(): Long {
-        val monkeys = getMokeys()
-        val root = monkeys.byName("root")
+    override fun part1() = MonkeyMath(input).solveFor("root")
 
-        while (root.result == null) {
-            monkeys.filter { it.result == null }.forEach { monkey ->
-                val op1 = monkeys.byName(monkey.op1!!).result
-                val op2 = monkeys.byName(monkey.op2!!).result
-                if (op1 != null && op2 != null) {
-                    monkey.result = monkey.op!!.doOperation(op1, op2)
+    override fun part2() = MonkeyMath(input).equalAt("root").transposeFor("humn").solveFor("humn")
+
+    class MonkeyMath(val input: String) {
+
+        private val monkeys = input.splitLines()
+            .map {
+                val words = it.split(" ")
+                val name = words[0].dropLast(1)
+                if (words.size == 2) {
+                    Monkey(name, result = words[1].toLong())
+                } else {
+                    Monkey(name, words[1], words.last(), Operation.of(words[2]))
                 }
-            }
-        }
-        return root.result as Long
-    }
+            }.toMutableList()
 
-    private fun String.opposite() = when (this) {
-        "+" -> "-"
-        "-" -> "+"
-        "*" -> "/"
-        "/" -> "*"
-        else -> throw Exception("unknown operation")
-    }
+        private fun List<Monkey>.byName(name: String?) = this.single { it.name == name }
 
-    override fun part2(): Any? {
-        var monkeys = getMokeys().toMutableList()
-        val root = monkeys.byName("root")
-        val p2 = monkeys.byName(root.op2!!)
-        while (p2.result == null) {
-            monkeys.filter { it.result == null }.forEach { monkey ->
-                val op1 = monkeys.byName(monkey.op1!!).result
-                val op2 = monkeys.byName(monkey.op2!!).result
-                if (op1 != null && op2 != null) {
-                    monkey.result = monkey.op!!.doOperation(op1, op2)
-                }
-            }
-        }
-        monkeys = getMokeys().toMutableList()
-//        println()
-//        p2.print()
-        monkeys.remove(root)
-        monkeys.remove(p2)
-        monkeys.remove(monkeys.byName("humn"))
-//        monkeys.add(Monkey(root.op1!!, result = p2.result))
-
-//        monkeys.forEach { it.print() }
-        val queue = ArrayDeque<String>()
-        queue.add("humn")
-
-        val monkeysTransposed = mutableListOf<Monkey>()
-
-        while (queue.isNotEmpty()) {
-            val monkeyName = queue.removeFirst()
-            if (monkeys.firstOrNull { it.name == monkeyName }?.result != null) continue
-
-            monkeys
-                .filter { it.op1 == monkeyName || it.op2 == monkeyName }
-                .filter { it.result == null }
-                .forEach { monkey ->
-                    val newMonkey = if (monkey.op in listOf("+", "*")) {
-                        if (monkeyName == monkey.op2) {
-                            Monkey(monkeyName, null, monkey.name, monkey.op1, monkey.op!!.opposite())
-                        } else {
-                            Monkey(monkeyName, null, monkey.name, monkey.op2, monkey.op!!.opposite())
-                        }
-                    } else {
-                        if (monkeyName == monkey.op2) {
-                            Monkey(monkeyName, null, monkey.op1, monkey.name, monkey.op)
-                        } else {
-                            Monkey(monkeyName, null, monkey.name, monkey.op2, monkey.op!!.opposite())
+        fun solveFor(monkeyName: String): Long {
+            val monkey = monkeys.byName(monkeyName)
+            while (monkey.result == null) {
+                monkeys.filter { it.result == null }
+                    .forEach { monkey ->
+                        val operand1 = monkeys.byName(monkey.operand1).result
+                        val operand2 = monkeys.byName(monkey.operand2).result
+                        if (operand1 != null && operand2 != null) {
+                            monkey.result = monkey.operation?.on(operand1, operand2)
                         }
                     }
-                    monkeysTransposed.add(newMonkey)
-                    monkeys.remove(monkey)
-                    queue.add(newMonkey.op1!!)
-                    queue.add(newMonkey.op2!!)
-                }
+            }
+            return monkey.result!!
         }
 
-        monkeys.addAll(monkeysTransposed)
-        monkeys.add(Monkey(root.op1!!, result = p2.result))
+        fun equalAt(string: String): MonkeyMath {
+            val equalityMonkey = monkeys.byName(string)
+            val equal = solveFor(equalityMonkey.operand2!!)
+            monkeys.reset()
+            monkeys.add(Monkey(equalityMonkey.operand1!!, result = equal))
+            return this
+        }
 
-        val humn = monkeys.byName("humn")
-        while (humn.result == null) {
-            monkeys.filter { it.result == null }.forEach { monkey ->
-                val op1 = monkeys.byName(monkey.op1!!).result
-                val op2 = monkeys.byName(monkey.op2!!).result
-                if (op1 != null && op2 != null) {
-                    monkey.result = monkey.op!!.doOperation(op1, op2)
+        fun transposeFor(string: String): MonkeyMath {
+            monkeys.remove(monkeys.byName(string))
+
+            val queue = ArrayDeque<String>()
+            queue.add(string)
+
+            val monkeysTransposed = mutableListOf<Monkey>()
+
+            while (queue.isNotEmpty()) {
+                val monkeyName = queue.removeFirst()
+                if (monkeys.firstOrNull { it.name == monkeyName }?.result != null) continue
+
+                monkeys
+                    .filter { it.operand1 == monkeyName || it.operand2 == monkeyName }
+                    .filter { it.result == null }
+                    .forEach { monkey ->
+                        val newMonkey = monkey.transposeFor(monkeyName)
+                        monkeysTransposed.add(newMonkey)
+                        monkeys.remove(monkey)
+                        queue.add(newMonkey.operand1!!)
+                        queue.add(newMonkey.operand2!!)
+                    }
+            }
+
+            monkeys.addAll(monkeysTransposed)
+            return this
+        }
+
+
+        private fun List<Monkey>.reset() = this.forEach { monkey ->
+            if (monkey.operation != null) monkey.result = null
+        }
+
+        data class Monkey(
+            val name: String,
+            val operand1: String? = null,
+            val operand2: String? = null,
+            val operation: Operation? = null,
+            var result: Long? = null
+        ) {
+            fun transposeFor(monkeyName: String): Monkey {
+                return when (monkeyName) {
+                    operand1 -> Monkey(monkeyName, name, operand2, operation!!.opposite)
+                    else -> when (operation) {
+                        Operation.Plus, Operation.Times -> Monkey(monkeyName, name, operand1, operation.opposite)
+                        else -> Monkey(monkeyName, operand1, name, operation)
+                    }
                 }
             }
         }
+    }
 
-//        3759566892641
 
-        return humn.result
+    enum class Operation {
+        Plus,
+        Minus,
+        Times,
+        DivededBy;
+
+        fun on(operand1: Long, operand2: Long) = when (this) {
+            Plus -> operand1 + operand2
+            Minus -> operand1 - operand2
+            Times -> operand1 * operand2
+            DivededBy -> operand1 / operand2
+        }
+
+        val opposite
+            get() = when (this) {
+                Plus -> Minus
+                Minus -> Plus
+                Times -> DivededBy
+                DivededBy -> Times
+            }
+
+        companion object {
+            fun of(string: String) = when (string) {
+                "+" -> Plus
+                "-" -> Minus
+                "*" -> Times
+                "/" -> DivededBy
+                else -> throw Exception("unknown operation: $string")
+            }
+        }
     }
 }
