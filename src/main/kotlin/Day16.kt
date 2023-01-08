a41756f1f7e1f1ace2f1ae862eb7b0db4bb814e5
Julian Baumgartner <julian.baumgaertner@bmw.de>
2023-01-08 19:36:21 +0100

    Day16 part2

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -5,20 +5,84 @@ fun main() {
     Day16().solve()
 }
 
-class Day16(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("", inputType = inputType) {
+class Day16(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("Proboscidea Volcanium", inputType = inputType) {
 
-    private val valves = input.splitLines().map {
-        it.split(" ").let { information ->
-            Valve(information[1],
-                information[4].split("""=|;""".toRegex())[1].toInt(),
-                information.slice(9 until information.size).map { it.removeSuffix(",") })
-        }
-    }
-
+    private val valves = input.parseValves()
     private val distances: MutableMap<String, MutableMap<String, Int>> = mutableMapOf()
 
     init {
-        valves.filter { it.rate > 0 || it.name == "AA" }.`forEach` { startValve ->
+        measureTunnelDistances()
+    }
+
+    override fun part1() = bfs(30, distances).pressure
+
+    override fun part2(): Int {
+        val time = 26
+
+        val person1 = bfs(time, distances, false)
+        val closedValves =
+            distances.map { (valve, distances) ->
+                valve to distances.filter { it.key !in person1.openedValves }
+            }.toMap()
+        val elephant = bfs(time, closedValves, false)
+
+        return person1.pressure + elephant.pressure
+    }
+
+    data class FlowManagement(val timeLeft: Int, val position: String, val opened: List<String> = emptyList(), val pressure: Int = 0)
+
+    data class Flow(val pressure: Int, val openedValves: List<String>)
+
+    data class Valve(
+        val name: String,
+        val rate: Int,
+        val tunnels: List<String>,
+    )
+
+    private fun bfs(initialTime: Int, distances: Map<String, Map<String, Int>>, part1: Boolean = true): Flow {
+        val queue = ArrayDeque<FlowManagement>()
+        queue.add(FlowManagement(initialTime, "AA"))
+
+        var maxPressure = 0
+        var visitedValves: List<String> = emptyList()
+
+        while (queue.isNotEmpty()) {
+
+            val (time, position, opened, pressure) = queue.removeFirst()
+
+            if (pressure > maxPressure) {
+                maxPressure = pressure
+                visitedValves = opened
+            }
+
+            if (time == 0) continue
+
+            val neighbours = distances[position]!!
+            neighbours
+                .filter { it.key !in opened }
+                .forEach { (valve, distance) ->
+
+                    val timeLeft = time - distance - 1
+                    val additionalPressure = valves.single { it.name == valve }.rate * if (part1 && timeLeft == 0) 1 else timeLeft
+
+                    if (timeLeft >= 0) {
+                        queue.add(
+                            FlowManagement(
+                                timeLeft,
+                                valve,
+                                opened + valve,
+                                pressure + additionalPressure
+                            )
+                        )
+                    }
+                }
+        }
+
+        return Flow(maxPressure, visitedValves)
+    }
+
+    private fun measureTunnelDistances() {
+        valves.filter { it.rate > 0 || it.name == "AA" }.forEach { startValve ->
 
             distances[startValve.name] = mutableMapOf()
 
@@ -37,54 +101,12 @@ class Day16(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("", inputType = inputType)
         }
     }
 
-    data class FlowManagement(val timeLeft: Int, val position: String, val opened: List<String> = emptyList(), val pressure: Int = 0)
-
-    override fun part1(): Int {
-        val queue = ArrayDeque<FlowManagement>()
-        queue.add(FlowManagement(30, "AA"))
-
-        var maxPressure = 0
-
-        while (queue.isNotEmpty()) {
-
-            val (time, position, opened, pressure) = queue.removeFirst()
-
-            if (pressure > maxPressure) maxPressure = pressure
-
-            if (time == 0) continue
-
-            val neighbours = distances[position]!!
-            neighbours
-                .filter { it.key !in opened }
-                .forEach { (valve, distance) ->
-
-                    val timeLeft = time - distance - 1
-                    val additionalPressure = valves.single { it.name == valve }.rate * if (timeLeft == 0) 1 else timeLeft
-
-                    if (timeLeft >= 0) {
-                        queue.add(
-                            FlowManagement(
-                                timeLeft,
-                                valve,
-                                opened + valve,
-                                pressure + additionalPressure
-                            )
-                        )
-                    }
-                }
+    private fun String.parseValves() = this.splitLines().map {
+        it.split(" ").let { information ->
+            Valve(information[1],
+                information[4].split("""=|;""".toRegex())[1].toInt(),
+                information.slice(9 until information.size).map { it.removeSuffix(",") })
         }
-
-        return maxPressure
     }
-
-    override fun part2(): Any? {
-        return "not yet implement"
-    }
-
-    data class Valve(
-        val name: String,
-        val rate: Int,
-        val tunnels: List<String>,
-    )
 
 }           
